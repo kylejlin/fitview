799cb1687b6ce1f5bf2ee2017c838d57b5b4be7d
Kyle Lin <kylejlin.us@gmail.com>
2019-07-14 12:04:50 -0700

    Display some activity info

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import EasyFit from "easy-fit";
 
 import Option from "./Option";
 
-class App extends React.Component {
+export default class App extends React.Component {
   constructor() {
     super();
 
@@ -28,7 +28,27 @@ class App extends React.Component {
 
         {this.state.file.match({
           none: () => <p>No file uploaded</p>,
-          some: value => (console.log(value), <p>File uploaded</p>)
+          some: value => {
+            console.log(value);
+
+            const records = getActivityRecords(value.activity);
+            const startDate = records[0].timestamp;
+            const endDate = records[records.length - 1].timestamp;
+
+            return (
+              <div className="File">
+                <div>Sport: {capitalizeFirstLetter(value.sport.sport)}</div>
+                <div>
+                  Date: {dayOfWeekString(startDate.getDay())}{" "}
+                  {startDate.getDate()} {monthString(startDate.getMonth())}{" "}
+                  {startDate.getFullYear()}
+                </div>
+                <div>Start time: {getTime(startDate)}</div>
+                <div>End time: {getTime(endDate)}</div>
+                <div>Total duration: {getActivityDuration(value.activity)}</div>
+              </div>
+            );
+          }
         })}
       </div>
     );
@@ -65,4 +85,66 @@ class App extends React.Component {
   }
 }
 
-export default App;
+function getActivityRecords(activity) {
+  return activity.sessions.map(session => getSessionRecords(session)).flat();
+}
+
+function getSessionRecords(session) {
+  return session.laps.map(lap => lap.records).flat();
+}
+
+function capitalizeFirstLetter(word) {
+  return word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
+}
+
+function dayOfWeekString(index) {
+  return [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ][index];
+}
+
+function monthString(index) {
+  return [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ][index];
+}
+
+function getTime(date) {
+  return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+}
+
+function getActivityDuration(activity) {
+  const hours = Math.floor(activity.total_timer_time / 3600);
+  const minutes = Math.floor((activity.total_timer_time % 3600) / 60);
+  const seconds = Math.floor(activity.total_timer_time % 60);
+  return (
+    zeroPad(hours, 2) + ":" + zeroPad(minutes, 2) + ":" + zeroPad(seconds, 2)
+  );
+}
+
+function zeroPad(value, minWidth) {
+  const string = "" + value;
+  if (string.length < minWidth) {
+    const deficit = minWidth - string.length;
+    return "0".repeat(deficit) + string;
+  } else {
+    return string;
+  }
+}
